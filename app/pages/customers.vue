ca2d65a7ef7ee1f9f9a4feb741e13d452eba98ba
xbabas55 <jhonkaito77@gmail.com>
2025-10-16 10:33:40 +0200

    fix(customers): remove trailing comma

--- app/pages/customers.vue
+++ app/pages/customers.vue
@@ -298,7 +298,7 @@ const pagination = ref({
           tbody: '[&>tr]:last:[&>td]:border-b-0',
           th: 'py-2 first:rounded-l-lg last:rounded-r-lg border-y border-default first:border-l last:border-r',
           td: 'border-b border-default',
-          separator: 'h-0',
+          separator: 'h-0'
         }"
       />
 
